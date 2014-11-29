8b060b8d368badb4da3e268f47fb84a5da09d0ae
Mehul Ira <mehulira@gmail.com>
2014-11-29 15:23:50 +0000

    analytics added

--- _site/app/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject app "1.0.0-static"
-  :main app.core
-  :description "static blog generator"
-  :dependencies [
-			[org.clojure/clojure "1.3.0"] 
-			[markdown-clj "0.9.58"]
-		]
-)
--- _site/app/src/app/core.clj
+++
@@ -1,5 +0,0 @@
-(ns app.core (:use markdown.core))
-
-(defn -main [& args] (print "Hello!")
-
-
