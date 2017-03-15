db584f311381db01f31deb9253a3a98f79743876
Mehul Irá <mehul.ira@mindera.com>
2017-03-15 00:14:31 +0000

    Added list.html

--- src/index.js
+++ src/index.js
@@ -27,13 +27,13 @@ const postFileParser = function(file, filePath) {
   const content = splittedContent[1];
   const post = JSON.parse(metadata);
   post.content = marked(content);
-  post.source = filePath;
   post.date = new Date(post.date);
+  post.targetPath = filePath.replace('.md', '.html').split('/')[2];
   return post;
 };
 
 const loadTemplate = function(path) {
-  const source = readFile(env.templates.post, i => i);
+  const source = readFile(path, i => i);
   return Handlebars.compile(source);
 };
 
@@ -43,5 +43,6 @@ const writePost = function(post, template, target) {
 
 const posts = readFiles(env.postDir, postFileParser);
 posts.sort((a, b) => b.date - a.date);
-posts.map(post => writePost(post, loadTemplate(env.templates.post), post => post.source.replace('.md', '.html').split('/')[2]));
+posts.map(post => writePost(post, loadTemplate(env.templates.post), post => post.targetPath));
 writePost(posts[0], loadTemplate(env.templates.post), post => 'index.html');
+fs.writeFileSync('list.html', loadTemplate(env.templates.postList)({ posts: posts }));
